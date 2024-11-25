7ed1d14275916c49aeb84f68004cdaa06a83587e
maksonbarbosa <makson_fernandes@hotmail.com>
2024-11-25 19:57:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Validação básica para o formulário
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    alert("Formulário enviado com sucesso!");
+    event.preventDefault();
+});
